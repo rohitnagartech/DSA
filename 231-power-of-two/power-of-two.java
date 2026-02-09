e7e55b92eb1d2d50e48ff9fb29d19ad8eca5cb05
Rohit Nagar <rohitnagartech124@gmail.com>
2026-02-09 09:53:31 +0530

    Time: 1 ms (95.69%) | Memory: 42.9 MB (15.08%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,19 +1,23 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if(n<1){
-            return false;
-        }else if(n==1){
-            return true;
-        }else{
-            while(n%2==0){
-                n = n/2;
+    public boolean isPowerOfTwo(int n) {      // function to check power of two
+
+        if (n < 1) {                          // if number is 0 or negative
+            return false;                     // not a power of two
+        } 
+        else if (n == 1) {                   // if number is exactly 1
+            return true;                     // 1 = 2^0 → power of two
+        } 
+        else {                               // for numbers greater than 1
+            while (n % 2 == 0) {             // while n is divisible by 2
+                n = n / 2;                   // keep dividing by 2
             }
 
-            if(n==1){
-                return true;
-            }else{
-                return false;
+            if (n == 1) {                    // if after division n becomes 1
+                return true;                 // then it's power of two
+            } 
+            else {                           // otherwise
+                return false;                // not a power of two
             }
         }
     }
-} 
+}
